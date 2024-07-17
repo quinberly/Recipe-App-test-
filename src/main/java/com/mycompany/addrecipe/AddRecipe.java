b8d7c85247a60e2f4f017bfc44b72db05f9348f7
anreh <anurehka@gmail.com>
2024-07-17 12:40:28 +0800

    completed add recipes (updated)

--- src/main/java/com/mycompany/addrecipe/AddRecipe.java
+++ src/main/java/com/mycompany/addrecipe/AddRecipe.java
@@ -6,5 +6,6 @@ public class AddRecipe {
     public static void main(String[] args) {
         System.out.println("Add recipes");
         System.out.println("Happy");
+        System.out.println("Welcome");
     }
 }
